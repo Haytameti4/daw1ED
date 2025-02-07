3ede999b5e5e84aa31986a5123542bb2337a7804
Haytam <iesvda.haybas@gmail.com>
2025-02-07 09:48:11 +0100

    Prueba de push

---
+++ HolaMundoApp.java
@@ -0,0 +1,12 @@
+//Aplicacion holamundo de ejemplo
+//
+
+class HolaMundoApp {
+	public statis void main ( String args[])
+	{
+		System.out.println ("Hola mundo!");
+		System.out.println ("Hola Git!");
+	}
+}
+
+
